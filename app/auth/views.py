9a95a6a11584ba9707da2d8c48736294e3b9ec85
aliasxu <1632789143@qq.com>
2017-06-07 17:39:43 +0800

    登陆/注册前端页面

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,7 +34,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Resister success!!')
-        return redirect(url_for('login'))
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html',form=form)
 
 
--- app/models.py
+++ app/models.py
@@ -13,11 +13,27 @@ class User(db.Model):
     def check_password(self,password):
         return self.password == password
 
+    @property
+    def is_authenticated(self):
+        return True
 
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return str(self.id)
+        except AttributeError:
+            raise NotImplementedError('No `id` attribute - override `get_id`')
 
 @lm.user_loader
 def load_user(userid):
-    return User.get(userid)
+    return User.query.get(userid)
 
 class Event(db.Model):
     __tablename__ = 'events'
